4e9039818f6b5dd9bc5c97bfdabf40c2fe86cb79
Ivan <yanja2008016@gmail.com>
2025-05-06 10:50:02 +0300

    task 7

---
+++ OOP/Task7.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class DynArr {
+	int* arr; int size;
+public:
+	DynArr() {//по умолч
+		size = 1;
+		int* arr = new int[size];
+	}
+	DynArr(int size) {//параметры
+		this->size = size;
+		int* arr = new int[size];
+	}
+	DynArr(DynArr&& obj) noexcept {//перемещение
+		arr = obj.arr;
+		obj.arr = nullptr;
+	}
+	~DynArr(){//деструктор
+	delete[] arr;
+	arr = nullptr;
+	}	
+	int& operator[](int index) {
+		return arr[index];
+	}
+	friend std::ostream& operator<<(std::ostream& out, DynArr obj);
+	
+};
+DynArr createArr(int size) {
+	return DynArr(size);
+}
+std::ostream& operator<<(std::ostream& out, DynArr obj) {
+	for (int i = 0; i < obj.size; i++){
+		out << obj[i] << " ";
+	}
+	return out;
+}
+
+int main() {
+	system("pause");
+	DynArr arr = createArr(8000000);
+	system("pause");
+	DynArr arr1(std::move(arr));
+	system("pause");
+}
