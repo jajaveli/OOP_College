92eb47b672941a992056dc01569fc23ce1c5bb65
Ivan <yanja2008016@gmail.com>
2025-05-20 10:59:43 +0300

    task 8 nedoed

--- OOP/Task8.cpp
+++ OOP/Task8.cpp
@@ -8,10 +8,10 @@ public:
 		std::cout << "Create n" << data << std::endl;
 		this->data = std::make_unique<int>(data);
 	}
-	int getData()const { return *data; }
+	int& getData()const { return *data; }
 	std::shared_ptr<Node> getNext()const { return next; }
-	std::shared_ptr<Node> setData(int data) { this->data = std::make_unique<int>(data);  return std::shared_ptr<Node>(this); }
-	std::shared_ptr<Node> setNext(std::shared_ptr <Node>next) { this->next = next; return std::shared_ptr<Node>(this); }
+	void setData(int data) { this->data = std::make_unique<int>(data);/* return std::shared_ptr<Node>(this); */ }
+	void setNext(std::shared_ptr <Node>next) { this->next = next;/*return std::shared_ptr<Node>(this); */ }
 	void delNode() { ; }
 	~Node() { std::cout << "Delete n" << *data << std::endl; }
 };
@@ -41,16 +41,37 @@ public:
 		{
 			if (id >= 0) {
 				std::shared_ptr<Node> temp = first; 
-				for (size_t i = 0; i <= id; i++){
-					if (temp)temp = temp->getNext();
-
-				}
-				return temp->getData();
+				for (size_t i = 0; i <= id; i++) if (temp) { temp = temp->getNext(); }else break;
+				if (temp)return temp->getData();
+			
+			throw std::out_of_range("Потекло по трубам");
 			}
 		}
 	}
 	void deleteNode(int id) {
-
+		std::shared_ptr<Node> temp = first;
+		for (int i = 0; i < id - 1; i++)
+		{
+			if (temp) { temp = temp->getNext(); }
+			else break;
+		}
+		if (temp == first) first = first->getNext();
+		if (temp)temp->setNext(temp -> getNext() -> getNext());
+	}
+	~List() {
+		if (first){
+			while (first) {
+				std::shared_ptr<Node>temp = first->getNext();
+				if (temp){
+					first->setNext(nullptr);
+					first.reset();
+					first = temp;
+				}
+				else{
+					first.reset();
+				}
+			}
+		}
 	}
 };
 
@@ -61,6 +82,8 @@ int main() {
 	l1.addElement(1);
 	l1.addElement(2);
 	l1.addElement(3);
+	//l1.deleteNode(0);
+	l1.~List();
 	std::cout << l1 << std::endl;
 
 }
